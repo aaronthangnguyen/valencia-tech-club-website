934a45b6cbaa124a552c3d0bc57148eb1a89e17f
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-09 13:31:08 -0500

    Add TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				'discord-100': '#5865F2',
+				'discord-200': '#4b60D2'
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
