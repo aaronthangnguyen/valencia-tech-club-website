5dcae43228a2d3765a31d3e7db46a03a9be00d64
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-09 18:22:22 -0500

    Add margin bottom to p

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	<title>Valencia Tech Club</title>
 </svelte:head>
 <h1 class="text-4xl font-bold">Valencia Tech Club</h1>
-<p>
+<p class="mb-4">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore aut vel nobis corporis impedit
 	mollitia necessitatibus obcaecati incidunt repudiandae, provident beatae aliquid delectus libero
 	dolore quis veniam excepturi itaque qui!
