5856fbedb9f919d835f9e8850de8183abf59dd31
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-09 18:19:09 -0500

    Add Index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,18 @@
+<script>
+	import Link from '../components/navigation/Link.svelte';
+	import DiscordLink from '../components/navigation/DiscordLink.svelte';
+</script>
+
 <svelte:head>
 	<title>Valencia Tech Club</title>
 </svelte:head>
-<h1>Index</h1>
+<h1 class="text-4xl font-bold">Valencia Tech Club</h1>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore aut vel nobis corporis impedit
+	mollitia necessitatibus obcaecati incidunt repudiandae, provident beatae aliquid delectus libero
+	dolore quis veniam excepturi itaque qui!
+</p>
+<div class="flex items-center gap-4">
+	<DiscordLink />
+	<Link href="/">Sign Up</Link>
+</div>
